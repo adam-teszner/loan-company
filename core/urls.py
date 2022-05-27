d1c7eaa46440f3b3310feddbb321e223ee8eb636
pyloslaw <pyloslaw@gmail.com>
2022-05-28 00:02:27 +0200

    Loan_co_site Created

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+# app_name = 'core'  nie wiem dlaczego nie dziala
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create_customer/', views.create_customer, name='create_customer'),
+    path('create_customer/add', views.CustomerCreateView.as_view(), name='add_customer'),
+    path('custom_create', views.custom_customer, name='custom_create'),
+]
