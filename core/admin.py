999c02bc0b51d0b9c377d4a3d50f5f115215fd4b
pyloslaw <pyloslaw@gmail.com>
2022-10-08 18:30:15 +0200

    changed db for postgres, in process of changing Product model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Customer, Adress, Workplace, Product, UserInfo
+from .models import (Customer, Adress, Workplace, Product, UserInfo,
+                    Payment)
+                    # ProductBankAccount)
 # Register your models here.
 
 admin.site.register(Adress)
@@ -7,13 +9,43 @@ admin.site.register(Workplace)
 
 
 
+
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'product_name', 'created_date', 'installments',
-                    'amount_requested', 'total_amount', 'loan_period']
+                    'amount_requested', 'total_amount', 'loan_period',                  
+                    
+                    ]
     readonly_fields = ['created_date', 'id', 'days_since_create',
                     'total_amount', 'installments',
-                    'installement_schedule', 'installment_dict']
+                    'installement_schedule', 'installment_dict',
+                    'get_payments',
+                    # 'payments_amounts',
+                    'paid_total',
+                    'paid_by_day',
+                    'inst_sch',
+                    'payments_human',
+                    'paid_by_day_human',
+                    ]
+'''
+class ProductBankAccAdmin(admin.ModelAdmin):
+    list_display = [
+        'product',
+        'account_no',
+        # 'pay_installment'
+    ]
 
+    readonly_fields = [
+        'paid_installments',
+        'paid_total',
+        # 'pay_installment',
+        'paid_total'
+    ]
+
+'''
+
+class PaymentAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_date']
 
 class UserInfoAdmin(admin.ModelAdmin):
     readonly_fields = ['created_date', 'id']
@@ -21,7 +53,8 @@ class UserInfoAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     readonly_fields = ['created_date', 'id']
 
-
+admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(UserInfo, UserInfoAdmin)
 admin.site.register(Customer, CustomerAdmin)
+# admin.site.register(ProductBankAccount, ProductBankAccAdmin)
