c4858111427e6aff3726417b9fff34e390c5b335
pyloslaw <pyloslaw@gmail.com>
2022-10-12 11:31:21 +0200

    changed Floats for Decimals in product, added payments, added installment schedule, delay in payemnts etc

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 import datetime
+from decimal import Decimal
 from email.policy import default
 import random
 from xml.sax.handler import property_declaration_handler
@@ -8,6 +9,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from dateutil.relativedelta import relativedelta
 from django.urls import reverse
 from django.contrib.auth.models import User
+from core.helper_methods import ProductMethods
 
 ## other
 
@@ -123,7 +125,7 @@ class Workplace(models.Model):
     def __str__(self):
         return f'{self.name}'
 
-class Product(models.Model):
+class Product(models.Model, ProductMethods):
 
     products = (
         ('L1', 'Loan'),
@@ -132,14 +134,25 @@ class Product(models.Model):
       
     owner = models.ForeignKey('Customer', on_delete=models.CASCADE,
                                     null=True, blank=True)
-    global_interest_rate = models.FloatField('Central banks\n interest rate in %', 
+    '''
+    ### old part ###
+    global_interest_rate = models.FloatField('Central banks\n interest rate in % - OLD', 
                                     validators=[MinValueValidator(0), 
                                     MaxValueValidator(30)], blank=False,
                                     default=5.25)
-    lombard_rate = models.FloatField('Central banks lombard\n interest rate in %',
+    lombard_rate = models.FloatField('Central banks lombard\n interest rate in % - OLD',
                                     validators=[MinValueValidator(0), 
                                     MaxValueValidator(30)], blank=False,
                                     default=5.75)
+    '''                                
+    global_interest_rate_dec = models.DecimalField('Central banks\n interest rate in %',
+                                    validators=[MinValueValidator(0),
+                                    MaxValueValidator(30)], blank=False, default=Decimal('5.25'),
+                                    max_digits=4, decimal_places=2)
+    lombard_rate_dec = models.DecimalField('Central banks lombard\n interest rate in %',
+                                    validators=[MinValueValidator(0),
+                                    MaxValueValidator(30)], blank=False, default=Decimal('5.75'),
+                                    max_digits=4, decimal_places=2)
     product_name = models.CharField(choices = products, max_length=2,
                                     default='L1')
     amount_requested = models.IntegerField(validators=[MinValueValidator(2000), 
@@ -148,107 +161,11 @@ class Product(models.Model):
     loan_period = models.IntegerField(choices=list(zip(range(6, 49), range(6, 49))),
                                     blank=False, default=24)
     created_date = models.DateField(auto_now_add=True)
-    
-    
-    @property
-    def days_since_create(self):
-        today = datetime.date.today()
-        try:
-            days_passed = today - self.created_date
-            days_passed_split = str(days_passed).split(' ')
-            return int(days_passed_split[0])
-        except:
-            return 0
- 
-    @property
-    def total_amount(self):
-        return round(self.installments*self.loan_period,2)
 
-
-    @property
-    def installments(self):
-              
-        p = (self.global_interest_rate/100*2)/12
-        nu = p*((1+p)**self.loan_period)
-        de = ((1+p)**self.loan_period)-1
-        total = self.amount_requested * (nu/de)
-        if self.product_name == 'L1':
-            return round(total + (0.15*total), 2)
-        else:
-            return round(total, 2)
-
-    def __str__(self):
-        return f'ID: {self.id}, amount: {self.amount_requested}, period: {self.loan_period}'
-
-    @property
-    def installement_schedule(self):
-        try:
-            z = []
-            for x in range(1, self.loan_period+1):
-                
-                z.append(f'{x} - amount: -- {self.installments} ---- Required by: -- {self.created_date + relativedelta(months=x)}')
-            return '\n'.join(z)
-            # return z
-        except:
-            return 'You must create a loan first ! '
-
-
-    @property
-    def installment_dict(self):
-        try:
-            installments = {}
-            for x in range(1, self.loan_period+1):
-                installments[x] = {
-                    'amount' : self.installments,
-                    'required by' : str(self.created_date + relativedelta(months=x))
-                }
-                # installments['amount'] = self.installments
-                # installments['required by'] = self.created_date + relativedelta(months=x)
-            return installments
-        except:
-            return 'NO Loans'
-
-    def inst_sch(self):
-        schedule = []
-        for x in range(1, self.loan_period+1):
-            schedule.append((x, self.installments, self.created_date + relativedelta(months=x)))
-        return schedule
-
-    @property
-    def get_payments(self):
-        payments = Payment.objects.filter(product=self.id)
-        payment_list = []
-        for payment in payments:
-            payment_list.append((payment.created_date, payment.amount))
-        return sorted(payment_list)
-
-
-    @property
-    def paid_total(self):
-        paid_total = 0
-        for (a,b) in self.get_payments:
-            paid_total = paid_total + b
-        return paid_total
-
-
-    @property
-    def paid_by_day(self):
-        daily_payements = {}
-        for a,b in self.get_payments:
-            if a in daily_payements:
-                daily_payements[a] += b
-            else:
-                daily_payements[a] = b
-        return daily_payements
-
-    def paid_by_day_human(self):
-        x = [f'{a} -- {b}' for (a,b) in self.paid_by_day.items()]
-        return '\n'.join(x)
-
-    def payments_human(self):
-        x = [f'{a} -- {b}' for (a,b) in self.get_payments]
-        return '\n'.join(x)
-        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.payments_query = Payment.objects.filter(product=self.id)
+        self.complete = []
 
 
 class Payment(models.Model):
@@ -256,96 +173,4 @@ class Payment(models.Model):
     created_date = models.DateField(auto_now_add=True)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
 
-'''
-
-    #### extending product 
-
-    account_no = models.IntegerField(default=account_generator)
-
-
-    # create balance field (how much is paid total) 
-    # paid_installments = []
-    paid_installments = ArrayField(
-        models.CharField(max_length=30), default=list, null=True, blank=True
-    )
-
-    # create ability to "pay" (like an ATM)
-
-    def pay_installment(self, amount):
-        date = str(datetime.date.today())
-        self.paid_installments.append((date, amount))
-
-    # sum all payments by day
-    @property
-    def payment_day_by_day(self):
-        daily_payements = {}
-        for k,v in self.paid_installments:
-            if k in daily_payements:
-                daily_payements[k] += v
-            else:
-                daily_payements[k] = v
-        return daily_payements
-    @property
-    def paid_total(self):
-        paid_total = 0
-        for k,v in self.paid_installments:
-            paid_total = paid_total + v
-        return paid_total
-
-    # connect payments to installements (product installements schedule)
-    # by tuples ? (ammount, date) ??
-
-    # create debt field(from schedule)
-
-    ### other, scoring, interest ?? ###    
-
-            
-
-####  Maybe use django-moeny ???
-
-class ProductBankAccount(models.Model):
-
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
-    account_no = models.IntegerField(default=account_generator)
-
-
-    # create balance field (how much is paid total) 
-    # paid_installments = []
-    paid_installments = ArrayField(
-        models.CharField(max_length=30), default=list
-    )
-
-    # create ability to "pay" (like an ATM)
-
-    def pay_installment(self, amount):
-        date = str(datetime.date.today())
-        self.paid_installments.append((date, amount))
-
-    # sum all payments by day
-
-    def payment_day_by_day(self):
-        daily_payements = {}
-        for k,v in self.paid_installments:
-            if k in daily_payements:
-                daily_payements[k] += v
-            else:
-                daily_payements[k] = v
-        return daily_payements
-
-    def paid_total(self):
-        paid_total = 0
-        for k,v in self.paid_installments:
-            paid_total + v
-        return paid_total
-
-    # connect payments to installements (product installements schedule)
-    # by tuples ? (ammount, date) ??
-
-    # create debt field(from schedule)
-
-    ### other, scoring, interest ?? ###    
-
-
-
-'''
 
